7f517cec6b85d9e746f01bb75f6f748c1d2b0031
Pavel Zagrebelin <pavel@zagrebelin.ru>
2017-08-02 10:10:26 +0500

    extract processing_update function

--- main.py
+++ main.py
@@ -10,26 +10,29 @@ import sys
 import telegram
 
 
-token = os.environ.get('BOT_TOKEN', None)
-if not token:
-    print('Please specify a BOT_TOKEN environment variable')
-    sys.exit()
-api = telegram.TelegramApi(token)
-offset = 0
+def process_update(api, update):
+    with open('log.txt', 'a', encoding='utf8') as log:
+        json.dump(update, log, ensure_ascii=False)
+        log.write('\n\n')
 
-log = open('log.txt', 'a', encoding='utf8')
 
-while True:
-    print(datetime.datetime.now(), end=' ', flush=True)
-    try:
-        ret = api.get_updates(offset=offset+1, timeout=30)
-        if ret['ok']:
-            updates = ret['result']
-            print(f'Got {len(updates)} updates.')
-            for update in updates:
-                offset = update['update_id']
-                log.write(json.dumps(update, ensure_ascii=False))
-                log.write('\n\n')
-                log.flush()
-    except Exception as e:
-        print(e)
+if __name__ == '__main__':
+    token = os.environ.get('BOT_TOKEN', None)
+    if not token:
+        print('Please specify a BOT_TOKEN environment variable')
+        sys.exit()
+    api = telegram.TelegramApi(token)
+    offset = 0
+
+    while True:
+        print(datetime.datetime.now(), end=' ', flush=True)
+        try:
+            ret = api.get_updates(offset=offset+1, timeout=30)
+            if ret['ok']:
+                updates = ret['result']
+                print(f'Got {len(updates)} updates.')
+                for update in updates:
+                    offset = update['update_id']
+                    process_update(api, update)
+        except Exception as e:
+            print(e)
